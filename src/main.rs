55180ef6ddda76125e8f2dd7af646c0bd83e0398
Jared Hance <jhance@dropbox.com>
2015-06-21 10:30:35 -0700

    Fix some annoyances

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let bucket = args.get(1).expect("need bucket");
+    let bucket_name = args.get(1).expect("need bucket");
     let file = args.get(2).expect("need file");
     let credentials = sss::Credentials::from_env();
     let connection = sss::Connection::new(credentials);
 
-    let bucket = connection.bucket("us-west-2", &bucket);
+    let bucket = connection.bucket("us-west-2", &bucket_name);
     match bucket.get(&file).contents() {
         Ok(contents) => { print!("{}", contents); }
         Err(e) => { println!("{:?}", e); }
