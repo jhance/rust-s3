262dc764bd5e4144ee9cbd7309a4b7a429742767
Jared Hance <jhance@dropbox.com>
2015-06-21 10:33:37 -0700

    allow specify region in test script

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let bucket_name = args.get(1).expect("need bucket");
     let file = args.get(2).expect("need file");
+    let region = args.get(3).unwrap_or("us-west-2");
     let credentials = sss::Credentials::from_env();
     let connection = sss::Connection::new(credentials);
 
-    let bucket = connection.bucket("us-west-2", &bucket_name);
+    let bucket = connection.bucket(&region, &bucket_name);
     match bucket.get(&file).contents() {
         Ok(contents) => { print!("{}", contents); }
         Err(e) => { println!("{:?}", e); }
